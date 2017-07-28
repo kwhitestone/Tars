e1fc16a5bd5c669dcf94caf5b13c9a25c38c6ef6
whitestone <kwhitestone@foxmail.com>
2017-07-28 10:09:36 +0800

    修改初始化脚本

--- build/init.sh
+++ build/init.sh
@@ -12,6 +12,8 @@ MysqlIp=$2
 MysqlUser=$3
 MysqlPass=$4
 
+yum install -y dos2unix rsync
+
 #copy 
 mkdir -p /usr/local/app/tars/
 cp framework.tgz tarsweb.tgz sql.tgz /usr/local/app/tars/
@@ -29,15 +31,30 @@ cp tarsweb/resin.xml /usr/local/resin/conf/
 /usr/local/resin/bin/resin.sh stop
 /usr/local/resin/bin/resin.sh start
 
+#sql
+ps -ef | grep tars | grep -v grep | kill -9 `awk '{print $2}'`
+tar -zxvf sql.tgz
+cd sql/sql/
+sed -i "s/192.168.2.131/${MachineIp}/g" `grep 192.168.2.131 -rl ./*`
+sed -i "s/db.tars.com/${MachineIp}/g" `grep db.tars.com -rl ./*`
+mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "grant all on *.* to 'tars'@'%' identified by 'tars2015' with grant option;"
+mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "grant all on *.* to 'tars'@'localhost' identified by 'tars2015' with grant option;"
+mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "grant all on *.* to 'tars'@'${MachineName}' identified by 'tars2015' with grant option;"
+mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "flush privileges;"
+dos2unix *.sh
+chmod u+x exec-sql.sh
+./exec-sql.sh
+cd -
+rm -rf sql/
+
 #cpp
 tar -zxvf framework.tgz
-rm -f app_log
-rm -f remote_app_log	
+rm -rf app_log
+rm -rf remote_app_log	
 sed -i "s/192.168.2.131/${MachineIp}/g" `grep 192.168.2.131 -rl ./* |grep -v "\\./init\\.sh" `
 sed -i "s/db.tars.com/${MachineIp}/g" `grep db.tars.com -rl ./* |grep -v "\\./init\\.sh" `
 sed -i "s/registry.tars.com/${MachineIp}/g" `grep registry.tars.com -rl ./* |grep -v "\\./init\\.sh" `
 sed -i "s/web.tars.com/${MachineIp}/g" `grep web.tars.com -rl ./* |grep -v "\\./init\\.sh" `
-yum install -y dos2unix
 find ./ -name "*.sh" | xargs dos2unix
 ln -s /data/tars/app_log app_log
 ln -s /data/tars/remote_app_log remote_app_log
@@ -45,15 +62,3 @@ chmod u+x tars_install.sh
 ./tars_install.sh
 ./tarspatch/util/init.sh
 
-#sql
-tar -zxvf sql.tgz
-cd sql
-sed -i "s/192.168.2.131/${MachineIp}/g" `grep 192.168.2.131 -rl ./*`
-sed -i "s/db.tars.com/${MachineIp}/g" `grep db.tars.com -rl ./*`
-mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "grant all on *.* to 'tars'@'%' identified by 'tars2015' with grant option;"
-mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "grant all on *.* to 'tars'@'localhost' identified by 'tars2015' with grant option;"
-mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "grant all on *.* to 'tars'@'${MachineName}' identified by 'tars2015' with grant option;"
-mysql -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} -e "flush privileges;"
-mysqldump -h${MysqlIp} -u${MysqlUser} -p${MysqlPass} db_tars > db_tars.sql
-cd ../
-#rm -rf sql/
